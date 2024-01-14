671d5c55bee07a57cde800f5beba0dc1d46c1439
Kameron Rodrigues <kameronr@email.arizona.edu>
2024-01-13 22:14:16 -0800

    fix indent syntax bug

--- chrombpnet/training/utils/losses.py
+++ chrombpnet/training/utils/losses.py
@@ -11,36 +11,36 @@ def multinomial_nll(true_percentages, pred_percentages):
       pred_percentages: predicted percent methylation values
     """
 
-avg_coverage=25
+	avg_coverage=25
 
-pre_alpha = np.round(avg_coverage*np.array(true_percentages))
-pre_beta = np.subtract(avg_coverage, pre_alpha)
+	pre_alpha = np.round(avg_coverage*np.array(true_percentages))
+	pre_beta = np.subtract(avg_coverage, pre_alpha)
 
-pre_alpha = np.add(pre_alpha, 0.00001)
-pre_beta = np.add(pre_beta, 0.00001)
+	pre_alpha = np.add(pre_alpha, 0.00001)
+	pre_beta = np.add(pre_beta, 0.00001)
 
-alpha_true = pre_alpha.tolist()
-beta_true = pre_beta.tolist()
+	alpha_true = pre_alpha.tolist()
+	beta_true = pre_beta.tolist()
 
-pred_percentages = np.array(pred_percentages)
+	pred_percentages = np.array(pred_percentages)
 
-#fix percentages that are 0 or 1 to a little more than 0 or a little less than 1.
-epsilon = 0.0000001
-zero_indices = pred_percentages == 0
-one_indices = pred_percentages == 1
-# Add epsilon to elements that are exactly 0
-pred_percentages[zero_indices] += epsilon
-# Subtract epsilon from elements that are exactly 1
-pred_percentages[one_indices] -= epsilon
+	#fix percentages that are 0 or 1 to a little more than 0 or a little less than 1.
+	epsilon = 0.0000001
+	zero_indices = pred_percentages == 0
+	one_indices = pred_percentages == 1
+	# Add epsilon to elements that are exactly 0
+	pred_percentages[zero_indices] += epsilon
+	# Subtract epsilon from elements that are exactly 1
+	pred_percentages[one_indices] -= epsilon
 
 
-dists = tfp.distributions.Beta(alpha_true, beta_true)
+	dists = tfp.distributions.Beta(alpha_true, beta_true)
 
-log_probs=dists.log_prob(pred_percentages)
-neg_sum_log_probs=-tf.reduce_sum(log_probs)
-average_NLL=neg_sum_log_probs/tf.cast(tf.shape(true_percentages)[0], dtype=tf.float32)
+	log_probs=dists.log_prob(pred_percentages)
+	neg_sum_log_probs=-tf.reduce_sum(log_probs)
+	average_NLL=neg_sum_log_probs/tf.cast(tf.shape(true_percentages)[0], dtype=tf.float32)
 
-return(average_NLL)
+	return(average_NLL)
 
 
 
